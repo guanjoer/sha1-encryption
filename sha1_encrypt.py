47a707e6d5105bd7573a5c43855a71cb7004af56
Youngmin Jo <54759844+designeryoungmin@users.noreply.github.com>
2024-09-11 19:52:05 +0900

    Add files

---
+++ sha1_encrypt.py
@@ -0,0 +1,12 @@
+import hashlib
+
+def mysql_password_hash(password):
+    # 1차 해싱 # 평문을 바이트 형식으로 변환 후, SHA 1 해싱 후, 16진수로 변환
+    hash1 = hashlib.sha1(password.encode()).hexdigest()
+    # 2차 해싱
+    hash2 = hashlib.sha1(bytes.fromhex(hash1)).hexdigest()
+    # MySQL 형식으로 변환
+    return "*" + hash2.upper()
+
+password = "hacked"
+print(mysql_password_hash(password)) 
